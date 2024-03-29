dfc291c887a5552c023f0b7b0050dd0e71b697cb
Benson "Santi" Isaac <bensonisonline@gmail.com>
2024-03-29 09:20:34 +0100

    Server ready

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const { default: helmet } = require("helmet")
+require("dotenv").config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.listen(PORT, () => {
+    console.log(`Server has started running at port http://localhost:${PORT}`)
+})
