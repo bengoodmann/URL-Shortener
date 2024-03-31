846965e29acd3212bb5de7f071abc59cf091d737
Benson "Santi" Isaac <bensonisonline@gmail.com>
2024-03-31 19:57:48 +0100

    API documentation done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,58 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/userModel");
-require("dotenv").config()
+require("dotenv").config();
 
+/**
+ * @swagger
+ * components:
+ *      schemas:
+ *        User:
+ *          type: object
+ *          required:
+ *               - name
+ *               - email
+ *               - password
+ *          properties:
+ *               name:
+ *                  type: string
+ *               email:
+ *                  type: string
+ *               password:
+ *                  type: string
+ */
+
+/**
+ * @swagger
+ * /api/v1/user/register:
+ *   post:
+ *     summary: Register a user
+ *     description: Register a new user with name, email, and password
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: "#/components/schemas/User"
+ *     responses:
+ *       '201':
+ *         description: User created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 User created:
+ *                   $ref: '#/components/schemas/User'
+ *       '400':
+ *         description: Bad request
+ *       '401':
+ *         description: Email is already registered
+ *       '500':
+ *         description: Internal server error
+ */
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -11,58 +61,96 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ error: "All fields are required" });
     }
     if (!isEmail(email)) {
-      return res.status(400).json({ error: "The email you entered is invalid" });
+      return res
+        .status(400)
+        .json({ error: "The email you entered is invalid" });
     }
     if (!isPasswordFormat(password)) {
       return res.status(400).json({
-        error:
-          `Wrong password format. 
+        error: `Wrong password format. 
           Your password must be 8 characters long, 
           contain at least 1 uppercase, 
           1 lowercase, 1 number 
           and a special character`,
       });
     }
-    const user = await User.findOne({ where: { email } })
+    const user = await User.findOne({ where: { email } });
     if (user) {
-      return res.status(401).json({ error: "Email is already registered" })
+      return res.status(401).json({ error: "Email is already registered" });
     }
-    const hashedPwd = await bcrypt.hash(password, 10)
-    const newUser = await User.create({ name, email, password: hashedPwd })
-    return res.status(201).json({ "User created": { newUser } })
+    const hashedPwd = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ name, email, password: hashedPwd });
+    return res.status(201).json({ "User created": { newUser } });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json("An unknown error has occurred", error)
+    console.error(error);
+    return res.status(500).json("An unknown error has occurred", error);
   }
 };
 
+/**
+ * @swagger
+ * /api/v1/user/login:
+ *   post:
+ *     summary: Login user
+ *     description: Authenticate user with email and password
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: "#/components/schemas/User"
+ *
+ *     responses:
+ *       '200':
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       '400':
+ *         description: Bad request
+ *       '500':
+ *         description: Internal server error
+ */
 const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     if (!(email && password)) {
-      return res.status(400).json({ email: "Email and password are required!" })
+      return res
+        .status(400)
+        .json({ email: "Email and password are required!" });
     }
-    const user = await User.findOne({ where: { email } })
+    const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(400).json({ error: "The user with the email doesn't exist" })
+      return res
+        .status(400)
+        .json({ error: "The user with the email doesn't exist" });
     }
-    const comparePwd = await bcrypt.compare(password, user.password)
+    const comparePwd = await bcrypt.compare(password, user.password);
     if (!comparePwd) {
-      return res.status(400).json({ error: "You entered a wrong password" })
+      return res.status(400).json({ error: "You entered a wrong password" });
     }
-    const token = jwt.sign({
-      user: {
-        id: user.id,
-        email: user.email
-      }
-    }, process.env.JWT_SECRET, { expiresIn: "31d" })
-    return res.status(200).json( { token } )
+    const token = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "31d" }
+    );
+    return res.status(200).json({ token });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json("An unknown error has occurred", error)
+    console.error(error);
+    return res.status(500).json("An unknown error has occurred", error);
   }
-
-}
+};
 
 const isEmail = (email) => {
   return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
@@ -72,4 +160,4 @@ const isPasswordFormat = (password) => {
   return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/.test(password);
 };
 
-module.exports = {registerUser, loginUser}
+module.exports = { registerUser, loginUser };
--- server.js
+++ server.js
@@ -1,29 +1,72 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const { default: helmet } = require("helmet")
-require("dotenv").config()
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { default: helmet } = require("helmet");
+require("dotenv").config();
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
-const DATABASE = require("./config/db")
-const { redirectShort } = require("./controllers/shortController")
+const DATABASE = require("./config/db");
+const { redirectShort } = require("./controllers/shortController");
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
 
-app.use(helmet())
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/api/v1/short", require("./routes/shortRoute"))
-app.use("/api/v1/user", require("./routes/userRoute"))
-app.get("/:short", redirectShort)
+const options = {
+  definition: {
+    openapi: "3.0.1",
 
-DATABASE
-    .sync()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server has started running at port http://localhost:${PORT}`)
-        })
-    })
+    info: {
+      title: "URL Shortener",
+      version: "1.0.0",
+      description: "A URL Shortener API built with expressJS",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./controllers/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use(
+  "/api/v1/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, {
+    explorer: true,
+    customCssUrl:
+      "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  })
+);
+
+app.use("/api/v1/short", require("./routes/shortRoute"));
+app.use("/api/v1/user", require("./routes/userRoute"));
+app.get("/:short", redirectShort);
+
+DATABASE.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server has started running at port http://localhost:${PORT}`);
+  });
+});
